1b8a872d5a36daa7960507a76eb08ccfba072e0c
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-05-11 12:43:00 +0900

    Initial Commit

---
+++ Sound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by 森田貴帆 on 2020/05/11.
+//  Copyright © 2020 森田貴帆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
